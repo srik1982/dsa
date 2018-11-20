368617a457714c2531013d3834d06597c71b1b03
srikanthrao <srikanthrao@192.168.0.102>
2018-11-20 13:41:41 +0530

    topsort

---
+++ DSA/src/skiena/graphs/DFS.java
@@ -0,0 +1,97 @@
+	package skiena.graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DFS {
+
+	public static void main(String[] args) {
+		Graph<Integer> g = new Graph<Integer>();
+		g.initGraph(8, true);
+		
+		g.insertEdge(0, 1);
+		g.insertEdge(0, 5);
+		g.insertEdge(1, 2);
+		g.insertEdge(2, 0);
+		g.insertEdge(2, 3);
+		g.insertEdge(5, 4);
+		g.insertEdge(5, 3);
+		g.insertEdge(5, 2);
+		g.insertEdge(4, 3);
+		g.insertEdge(3, 5);
+		g.insertEdge(6, 7);
+		
+//		g.printGraph();
+//		BFS bfs = new BFS();
+//		bfs.traverse(g, 0);
+		
+		DFS dfs = new DFS();
+		dfs.parent = new int[g.getnVertices()];
+		dfs.startTime = new int[g.getnVertices()];
+		dfs.endTime = new int[g.getnVertices()];
+		for(int i=0;i<g.getnVertices();i++) {
+			dfs.parent[i] = -1;
+		}
+		
+		dfs.traverse(g,0);
+		
+//		System.out.println("\n\n");
+//		for(int i=0;i<g.getnVertices();i++) {
+//			System.out.println("\n parent of "+i+" : "+dfs.parent[i]);
+//			System.out.println(" startTime of "+i+" : "+dfs.startTime[i]);
+//			System.out.println(" endTime of "+i+" : "+dfs.endTime[i]);
+//		}
+	}
+	
+	Set<Integer> visited = new HashSet<Integer>();
+	Set<Integer> processed = new HashSet<Integer>();
+	int[] parent;
+	int[] startTime,endTime;
+	int clock = 0;
+	
+	public void visit(int node) {
+		visited.add(node);
+		System.out.println("Visited "+node+" through edge: "+parent[node]+" -->"+node);
+	}
+	
+	public void process(int node) {
+		processed.add(node);
+		System.out.println("Processed "+node+". Total children = "+((endTime[node]-startTime[node])/2));
+	}
+	
+	//stupid code, we can just check if one of the nodes is visited earlier.
+	public boolean isCycle(int x, int y) {
+		boolean retVal = false;
+		int temp = x;
+		while(parent[temp]!=-1 && temp!=y) {
+			temp = parent[temp];
+		}
+		retVal = temp == y;
+		return retVal;
+	}
+	
+	public void traverse(Graph<Integer> g, int x) {		
+		
+			startTime[x] = clock++;
+			visit(x);
+			
+			VertexNode<Integer> vertexNode = g.getVertices().get(x);
+			List<VertexNode<Integer>> adjList = vertexNode.getAdjacencyList();
+			for(VertexNode<Integer> _v : adjList) {
+				if(!visited.contains(_v.getValue())) {
+					parent[_v.getValue()] = x;
+					traverse(g,_v.getValue());
+				}else if(isCycle(x,_v.getValue())) {
+					System.out.println("Cycle detected : "+x+" --> "+_v);
+				}
+			}
+			
+			endTime[x] = clock++;
+			process(x);
+		}
+	
+
+}
---
+++ DSA/src/skiena/graphs/TopologicalSort.java
@@ -0,0 +1,59 @@
+package skiena.graphs;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TopologicalSort {
+
+	public static void main(String[] args) {
+		Graph<Integer> graph = new Graph<Integer>();
+		graph.initGraph(8, true);
+		graph.insertEdge(0, 2);
+        graph.insertEdge(0, 1);
+        graph.insertEdge(2, 3);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+        graph.insertEdge(2, 7);
+        graph.insertEdge(7, 10);
+        
+        TopologicalSort ts = new TopologicalSort();
+        
+        Deque<VertexNode<Integer>> topSortedNodes = ts.topSort(graph);
+        
+        while(!topSortedNodes.isEmpty()) {
+        	System.out.println(topSortedNodes.removeFirst().getValue());
+        }
+        
+	}
+	/**
+	 * Note that I am using Deque to implement a stack for 2 reasons -
+	 * 1. Java Deque implementation says that we should preper this interface over Stack
+	 * 2. Deque provides addFirst(e), removeFirst() and peekFirst() which are equivalent to push(), pop() and peek()
+	 * @param g
+	 * @return
+	 */
+	public Deque<VertexNode<Integer>> topSort(Graph<Integer> g){
+		Deque<VertexNode<Integer>> stack = new ArrayDeque<VertexNode<Integer>>();
+		Set<VertexNode<Integer>> visited = new HashSet<VertexNode<Integer>>();
+		for(VertexNode<Integer> v : g.getVertices())
+		{
+			if(!visited.contains(v)) {
+				topSortUtil(v,stack,visited);
+			}
+		}
+		return stack;
+	}
+	
+	public void topSortUtil(VertexNode<Integer> vertex, Deque<VertexNode<Integer>> stack, Set<VertexNode<Integer>> visited) {
+		visited.add(vertex);
+		for(VertexNode<Integer> adjNode : vertex.getAdjacencyList()) {
+			if(!visited.contains(adjNode)) {
+				topSortUtil(adjNode, stack, visited);
+			}
+		}
+		stack.addFirst(vertex);
+	}
+
+}
